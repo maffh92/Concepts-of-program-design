f3ca01dc4b049777b7ab7bf76e5f5c732dfe4ae9
Carlos Tomé <carlostome1990@gmail.com>
2017-01-13 16:34:35 +0100

    Clean up of the repository

--- Development/ProjectExamples/src/HelloWorld.scala
+++
@@ -1,6 +0,0 @@
-/**
-  * Created by maffh on 12/3/16.
-  */
-object HelloWorld extends App{
-  def getName = "Matthews"
-}
--- Development/ProjectExamples/src/ImplicitTree/BTree.scala
+++
@@ -1,6 +0,0 @@
-package ImplicitTree
-
-/**
-  * Created by maffh on 12/4/16.
-  */
-
