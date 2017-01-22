3cd0dc34c1aeebf98ac173086ac675742b3d7779
Carlos Tomé <carlostome1990@gmail.com>
2017-01-22 16:13:18 +0100

    Added gitignore and cleanup of class files unneeded

---
+++ Development/Generic/src/test/scala/CollectTest.scala
@@ -0,0 +1,8 @@
+import org.scalatest.FlatSpec
+import Functions.CollectObject._
+
+class CollectTest extends FlatSpec {
+  "Collect" should "get all integers from the list" in {
+    //assert(collect(List(Left(1), Right(true), Left(2))) == List(1,2))
+  }
+}
--- Development/ShapelessBenchmark/build.sbt
+++ Development/ShapelessBenchmark/build.sbt
@@ -1,4 +1,4 @@
-name := "shapeless-benchmark"
+name := "ShapelessBenchmark"
 
 version := "1.0"
 
@@ -7,4 +7,3 @@ scalaVersion := "2.12.1"
 libraryDependencies ++= Seq(
   "com.chuusai" %% "shapeless" % "2.3.2"
 )
-
