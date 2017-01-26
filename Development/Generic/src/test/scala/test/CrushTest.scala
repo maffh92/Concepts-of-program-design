9a2309cb3272465f095709d558e206962af7a30f
Matthew Swart <m.a.swart@students.uu.nl>
2017-01-26 21:49:39 +0100

    Moved the crush and map tests to the test directory

--- Development/Generic/src/test/scala/test/CrushTest.scala
+++ Development/Generic/src/test/scala/test/CrushTest.scala
@@ -1,7 +1,11 @@
 package test
 
 import org.scalatest.FlatSpec
+import Data.List._
 
-class CrushTest extends FlatSpec {
-
+class CrushTest extends FlatSpec{
+  //test the sum function
+  assert(Functions.Crush.sum(List(5,3,10))==18)
+  //test the product function
+  assert(Functions.Crush.product(List(5,3,10))==150)
 }
--- Development/Generic/src/test/scala/test/MapTest.scala
+++ Development/Generic/src/test/scala/test/MapTest.scala
@@ -1,7 +1,31 @@
 package test
 
 import org.scalatest.FlatSpec
+import Data.PerfectTree._
+import Data.Perfect
+import Data.Zero
+import Data.Fork
+import Data.Succ
+import Data.Tree._
+import Data.List._
+import Data.BinTree
+import Data.Bin
+import Data.Leaf
+import Functions.Map._
 
-class MapTest extends FlatSpec {
+class MapTest extends FlatSpec{
+  //increase function is used in the asserts to test if the map functions works
+  def increase(x : Int) = x + 1
 
+  //Binary tree test
+  val bintree : BinTree[Int] = Bin(Bin(Leaf(1),Leaf(2)),Bin(Leaf(3),Leaf(4)))
+  assert(map(increase)(bintree)== Bin(Bin(Leaf(2),Leaf(3)),Bin(Leaf(4),Leaf(5))))
+
+  //perfecttree test
+  val perfectTree : Perfect[Int] = Succ(Zero(Fork(1,2)))
+  assert(map(increase)(perfectTree) == Succ(Zero(Fork(2,3))))
+
+  //List test
+  assert(map(increase)(List(1,2)) == List(2,3))
+  assert(map(increase)(List(1,10,3,100)) == List(2,11,4,101))
 }
